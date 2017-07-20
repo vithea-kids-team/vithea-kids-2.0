892db91cd01aab77b0fba587ba61962ef144c814
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2017-07-20 17:34:20 +0100

    no message

--- client/src/app/models/caregiver.ts
+++ client/src/app/models/caregiver.ts
@@ -1,10 +1,10 @@
 export class Caregiver {
-   	public id: number
-	public firstName: string	
-	public lastName: string
-	public gender: string
-	public username: string 
-	public password: string
-	public confirmpassword: string
+    public id: number
+    public firstName: string
+    public lastName: string
+    public gender: string
+    public username: string
+    public password: string
+    public confirmpassword: string
     public email: string
 }
--- client/src/app/models/sequence.ts
+++ client/src/app/models/sequence.ts
@@ -1,7 +1,7 @@
 export class Sequence {
     public id: number;
     public sequenceName: string;
-    public childId : number;
-    public exercisesToAdd : Array<number>;
-    public childrenToAssign : Array<number>;
+    public childId: number;
+    public exercisesToAdd: Array<number>;
+    public childrenToAssign: Array<number>;
 }
