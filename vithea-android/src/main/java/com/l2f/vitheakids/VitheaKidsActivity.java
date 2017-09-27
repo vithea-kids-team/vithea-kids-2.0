d232acf9569aa49527241b0a82308d8d1a038a42
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2017-09-27 16:15:45 +0100

    #51 Child's app - implement reinforcement strategies (or hide them temporarily in the caregiver)

--- vithea-android/src/main/java/com/l2f/vitheakids/VitheaKidsActivity.java
+++ vithea-android/src/main/java/com/l2f/vitheakids/VitheaKidsActivity.java
@@ -727,6 +727,7 @@ public class VitheaKidsActivity extends AppCompatActivity {
                 if(attempts == 0) {
                     setReinforcementView();
                 }
+                else nextExerciseHandler();
                 break;
             case "OFF": // TODO OFF Reinforcement
             default:
@@ -792,12 +793,11 @@ public class VitheaKidsActivity extends AppCompatActivity {
                         new ReadTask().execute(answer);
                     }
                 }
+                else {
+                    new ReadTask().execute("Tenta outra vez.");
+                }
             }
         }
-
-        //if(!child.getPrompting().getPromptingRead() && isTextExercise){
-        //    new ReadTask().execute("Tenta outra vez."); // TODO Manter ou não ? Devia estar em string?
-        //}
     }
     private void previousExerciseHandler() {
         this.currentExercisePosition--;
